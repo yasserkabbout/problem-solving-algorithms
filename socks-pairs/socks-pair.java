c3bbc67266a41618cc460534130bb58b62418ff5
Yasser El Kabbout <yasserkabbout@gmail.com>
2018-11-27 01:25:23 +0300

    Create socks-pair.java

---
+++ socks-pairs/socks-pair.java
@@ -0,0 +1,60 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SocksParis {
+
+
+    public static void main(String[] args) {
+
+        int[] ar = {10, 20, 20, 10, 10, 30, 50, 10, 20,50,10,10,60,60,70,70,80};
+
+
+        Map<Integer, Integer> socksMap = new HashMap<>();
+        Map<Integer, Integer> pairsOfSocks = new HashMap<>();
+        int pairsCount =0;
+
+        for (int i : ar) {
+            if (socksMap.containsKey(i)) {
+                socksMap.put(i, socksMap.get(i) + 1);
+
+            } else {
+                socksMap.put(i, 1);
+            }
+
+        }
+
+        for (Map.Entry<Integer, Integer> entry : socksMap.entrySet()) {
+
+            //System.out.println(entry.getKey() + ": " + entry.getValue());
+
+
+            if (entry.getValue() % 2 == 0) {
+                //System.out.println(entry.getValue());
+                //System.out.println(entry.getValue()/2);
+                pairsOfSocks.put(entry.getKey(), (entry.getValue() / 2));
+
+            }
+
+             if(entry.getValue() % 2 == 1){
+                //System.out.println(entry.getValue());
+                 //System.out.println((entry.getValue()-1)/2);
+                pairsOfSocks.put(entry.getKey(), ((entry.getValue())/2));
+            }
+
+
+
+
+        }
+
+        for (Map.Entry<Integer, Integer> entry : pairsOfSocks.entrySet()) {
+
+
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+            pairsCount+=entry.getValue();
+
+        }
+
+        System.out.println("There is: "+ pairsCount+ " Pairs");
+    }
+}
+
